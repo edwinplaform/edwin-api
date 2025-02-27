382f2224f1f229ba97c510cb1b06c327f6e1a96f
Yethum Danith <yethumdanith2002@gmail.com>
2025-02-27 22:04:43 +0530

    add session management

--- app/models/session.model.js
+++ app/models/session.model.js
@@ -1,68 +1,154 @@
-import {DataTypes} from "sequelize";
+// import { DataTypes } from "sequelize";
+
+// export default (sequelize) => {
+//   const SessionModel = sequelize.define(
+//     "Session",
+//     {
+//       id: {
+//         type: DataTypes.STRING,
+//         primaryKey: true,
+//         unique: true,
+//       },
+//       appointmentId: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         references: {
+//           model: "Appointments",
+//           key: "id",
+//         },
+//       },
+//       studentId: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         references: {
+//           model: "Users",
+//           key: "userId",
+//         },
+//       },
+//       tutorId: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         references: {
+//           model: "Users",
+//           key: "userId",
+//         },
+//       },
+//       subject: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//       },
+//       date: {
+//         type: DataTypes.DATE,
+//         allowNull: false,
+//       },
+//       startTime: {
+//         type: DataTypes.TIME,
+//         allowNull: false,
+//       },
+//       endTime: {
+//         type: DataTypes.TIME,
+//         allowNull: false,
+//       },
+//       zoomLink: {
+//         type: DataTypes.STRING,
+//         allowNull: true,
+//       },
+//       materialUrl: {
+//         type: DataTypes.JSON,
+//         allowNull: true,
+//       },
+//       status: {
+//         type: DataTypes.ENUM(
+//           "PENDING",
+//           "SCHEDULED",
+//           "IN_PROGRESS",
+//           "COMPLETED",
+//           "CANCELLED"
+//         ),
+//         defaultValue: "PENDING",
+//       },
+//     },
+//     {
+//       timestamps: true,
+//     }
+//   );
+
+//   return SessionModel;
+// };
+
+import { DataTypes } from "sequelize";
 
 export default (sequelize) => {
-    const SessionModel = sequelize.define("Session", {
-        id: {
-            type: DataTypes.STRING,
-            primaryKey: true,
-            unique: true,
+  const SessionModel = sequelize.define(
+    "Session",
+    {
+      id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        unique: true,
+      },
+      appointmentId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+          model: "Appointments",
+          key: "id",
         },
-        appointmentId: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            references: {
-                model: 'Appointments',
-                key: 'id',
-            }
+      },
+      studentId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
         },
-        studentId: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            references: {
-                model: 'Users',
-                key: 'userId',
-            }
+      },
+      tutorId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
         },
-        tutorId: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            references: {
-                model: 'Users',
-                key: 'userId',
-            }
-        },
-        subject: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        date: {
-            type: DataTypes.DATE,
-            allowNull: false,
-        },
-        startTime: {
-            type: DataTypes.TIME,
-            allowNull: false,
-        },
-        endTime: {
-            type: DataTypes.TIME,
-            allowNull: false,
-        },
-        zoomLink: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        materialUrl: {
-            type: DataTypes.JSON,
-            allowNull: true,
-        },
-        status: {
-            type: DataTypes.ENUM('PENDING', 'SCHEDULED', 'IN_PROGRESS', 'COMPLETED', 'CANCELLED'),
-            defaultValue: 'PENDING'
-        }
-
-    }, {
-        timestamps: true
-    });
-
-    return SessionModel;
-}
+      },
+      subject: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      startTime: {
+        type: DataTypes.TIME,
+        allowNull: false,
+      },
+      endTime: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      zoomLink: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      materialUrl: {
+        type: DataTypes.JSON,
+        allowNull: true,
+      },
+      status: {
+        type: DataTypes.ENUM(
+          "PENDING",
+          "SCHEDULED",
+          "IN_PROGRESS",
+          "COMPLETED",
+          "CANCELED"
+        ),
+        defaultValue: "PENDING",
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+  return SessionModel;
+};
--- app/routes/session.js
+++ app/routes/session.js
@@ -1,17 +1,20 @@
 import express from "express";
 import {
-    createSession,
-    updateSession,
-    getTutorSession,
-    updateSessionStatus, getPaidSessionsForStudent
+  createSession,
+  updateSession,
+  getTutorSession,
+  updateSessionStatus,
+  getPaidSessionsForStudent,
+  getUserSessions,
 } from "../controllers/session.controller.js";
 
 const router = express.Router();
 
-router.post('/sessions', createSession);
-router.put('/sessions/:sessionId', updateSession);
-router.get('/tutors/:tutorId/sessions', getTutorSession);
-router.patch('/sessions/:sessionId/status', updateSessionStatus)
-router.get('/students/:studentId/sessions/paid', getPaidSessionsForStudent);
+router.get("/sessions/:userId", getUserSessions);
+router.post("/sessions", createSession);
+router.put("/sessions/:sessionId", updateSession);
+router.get("/tutors/:tutorId/sessions", getTutorSession);
+router.patch("/sessions/:sessionId/status", updateSessionStatus);
+router.get("/students/:studentId/sessions/paid", getPaidSessionsForStudent);
 
 export default router;
