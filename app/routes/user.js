38302e45363ea5c0918d9c58d62538a5815fbaf3
PramukaNavodh <pramunavodh@gmail.com>
2025-03-07 21:55:59 +0530

    added email templates. still not connected.

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { sendEmail } from '../service/emailService.js';
+
 import {
     createUser,
     deleteUser,
@@ -11,43 +11,23 @@ import {
     updateTutorStatus,
     filterUsers,
     updateBankDetails
+
 } from "../controllers/user.controller.js";
+
+// import {validateUser} from "../middleware/validationMiddleware.js";
+
+// import {validateResults} from "../middleware/validationResult.js";
 
 const router = express.Router();
 
 router.post('/upgradeRole', upgradeRole);
-
-router.post('/', async (req, res) => {
-    try {
-        const user = await createUser(req, res); // Assuming createUser returns the created user object
-
-        // Trigger welcome email
-        await sendEmail(user.email, 'Welcome to EDWin!', 'welcomeEmail', { username: user.name });
-
-        res.status(201).json({ message: "User created successfully", user }); //send success response.
-    } catch (error) {
-        console.error("Error creating user:", error);
-        res.status(500).json({ message: "Internal server error" });
-    }
-});
-
+router.post('/', createUser);
 router.get("/:userId", getUserById);
 router.put("/:userId", updateUser);
 router.delete("/:userId", deleteUser);
 router.get("/", listUser);
 router.get("/tutors/status/:status", getTutorsByStatus);
-
-router.patch("/tutors/:userId/status", async (req, res) => {
-    try{
-        const updatedTutor = await updateTutorStatus(req,res);
-        if(updatedTutor.status === "approved"){
-            await sendEmail(updatedTutor.email, 'Your Tutor Profile Has Been Approved!', 'tutorApprovalEmail', { tutorName: updatedTutor.name });
-        }
-    }catch(error){
-        console.error("error updating tutor status", error)
-    }
-});
-
+router.patch("/tutors/:userId/status", updateTutorStatus);
 router.get("/filter", filterUsers);
 router.patch('/tutors/:userId/bank', updateBankDetails);
 
